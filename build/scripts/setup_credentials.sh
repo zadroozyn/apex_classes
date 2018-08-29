78664abc85bc111aff448dc5ec5ae544ef542bbf
Michał Zadrużyński <michal@on-running.com>
2018-08-29 14:12:19 +0200

    irgne;gn

--- build/scripts/setup_credentials.sh
+++ build/scripts/setup_credentials.sh
@@ -1,6 +1,6 @@
 cat >build.properties <<EOF
 sf.username = $SFDC_USERNAME
 sf.password = $SFDC_PASSWORD$SFDC_TOKEN
-sf.serverurl = $SFDC_URL
+sf.serverurl = https://test.salesforce.com
 sf.maxPoll = 20
 EOF
